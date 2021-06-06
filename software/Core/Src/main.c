75eac36257e79e2cb3b3cd7adba07cf55f810ed4
smdjeff <smdjeff@gmail.com>
2021-06-06 11:31:32 -0600

    move from degrees to level based system.

--- software/Core/Src/main.c
+++ software/Core/Src/main.c
@@ -19,25 +19,34 @@ static void MX_GPIO_Init(void);
 
 #define HAL_GPIO_ReadPin(port,pin)          LL_GPIO_IsInputPinSet(port,pin)
 #define HAL_GPIO_WritePin(port,pin,value)   do { if(value) LL_GPIO_SetOutputPin(port,pin); else LL_GPIO_ResetOutputPin(port,pin); } while(0)
+#define HAL_GPIO_TogglePin(port,pin)        do { HAL_GPIO_WritePin(port, pin, !HAL_GPIO_ReadPin(port,pin) ); } while(0)
 
 #define HAL_GetTick()                       (systick)
 // tuned at 10s, assumes 32MHz sysclock
 #define delayUs(us)                         for (int i=0;i<us*6;i++){ asm("nop"); }
+#define delayMs(ms)                         do { delayUs((ms)*1000); } while(0)
 #define SECONDS_TO_TICKS(s)                 ((s)*1000)
 #define MINUTES_TO_TICKS(s)                 (SECONDS_TO_TICKS(s)*60)
 
 // tuned on 6/1/2021
+/*
+stepsPerRotation:200
+stepsPerInch:632.911392
+stepsPerLevel:1107.594937
+stepsPerCamDegree:12.306610
+stepsPerPercentOfLevel: 11.07594937
+*/
 #define rodInchesPerRotation                0.316
 #define stepsPerRotation                    200
-#define stepsPerInch                        (stepsPerRotation / rodInchesPerRotation)
+#define stepsPerInch                        (stepsPerRotation / rodInchesPerRotation) 
 #define stepsPerLevel                       ((3.5/2) * stepsPerInch )
 #define stepsPerCamDegree                   (stepsPerLevel / 90)
+#define stepsPerPercentOfLevel              (stepsPerLevel / 100)
 
 #define STEP_RAMP                           4
 #define STEP_SIZE                           400
 
 
-static void optoSimulator(void);
 
 static volatile uint32_t systick = 0;
 // called from stm32l0xx_it.c weak link ISR
@@ -63,23 +72,64 @@ void myIRQ_4_15(void) {
 
 
 typedef enum {
-    left    = 0,
-    right   = 1,
-    limit   = 2
+    but_left    = 0,
+    but_right   = 1,
+    but_limit   = 2
 } button_t;
 
 typedef enum {
-    cw = 1,
-    ccw = -1
-} direction_t;
+    step_enable = 0,
+    step_disable = 1
+} step_enabled_t;
+
+typedef enum {
+    step_assert = 0,
+    step_deassert = 1
+} step_reset_t;
+
+typedef enum {
+    step_dir_up = 0,
+    step_dir_down = 1
+} step_dir_t;
+
+typedef enum {
+    step_size_full  = 0,
+    step_size_half  = 1,
+    step_size_4th   = 2,
+    step_size_8th   = 3,
+    step_size_16th  = 4,
+    step_size_32nd  = 5
+}  step_size_t;
+ 
+typedef enum {
+    cam_dir_cw = 0,
+    cam_dir_ccw = 1
+} cam_dir_t;
+
+typedef enum {
+    opto_open = 0,
+    opto_closed = 1
+} opto_t;
+
+typedef enum {
+    level_down  = 0,
+    level_mid_r = 1,
+    level_up    = 2,
+    level_mid_l = 3,
+} level_t;
+
+static int step_percentage = 0;
+static int step_percentage_toggle = 0;
+static level_t current_level = 0;
+
 
 static uint8_t readPin(button_t id) {
   switch ( id ) {
-    case left:
+    case but_left:
       return HAL_GPIO_ReadPin( SW0_GPIO_Port, SW0_Pin ); 
-    case right: 
+    case but_right: 
       return HAL_GPIO_ReadPin( SW1_GPIO_Port, SW1_Pin ); 
-    case limit: 
+    case but_limit: 
       return HAL_GPIO_ReadPin( LIMIT_GPIO_Port, LIMIT_Pin ); 
   }
   return 0;
@@ -108,20 +158,6 @@ static bool debouncedSwitch(button_t id) {
     return false; // button released or button hasn't changed
 }
 
-static int camSteps = 0;
-static direction_t camDirection = 0;
-
-static void stepDirection(direction_t direction) {
-  camDirection = direction;
-  // int a = camSteps;// / (int)stepsPerCamDegree;  // approx 12 per degree.
-  // if ( a < (int)(0*stepsPerCamDegree) )   { a += (int)(360*stepsPerCamDegree); }
-  // if ( a > (int)(360*stepsPerCamDegree) ) { a -= (int)(360*stepsPerCamDegree); }
-  // if ( a >= (int)(175*stepsPerCamDegree) ) {
-  //   // the linear mechnaism only moves 180 degrees, and then reverses 
-  //   direction = -direction;
-  // }
-  HAL_GPIO_WritePin( S_DIR_GPIO_Port, S_DIR_Pin, (direction==cw) ? 0:1 );
-}
 
 static void stepSingle(void) {
   delayUs( STEP_SIZE );
@@ -129,12 +165,17 @@ static void stepSingle(void) {
   delayUs( STEP_SIZE );
   LL_GPIO_ResetOutputPin( S_STEP_GPIO_Port, S_STEP_Pin );
   
-  camSteps += camDirection;
-  // only really need to do this every 11 degrees or so (aka stepsPerCamDegree:12.306610 x 11 = 135 steps)
-  optoSimulator(); 
+  static int steps = 0;
+  if ( steps++ >= stepsPerPercentOfLevel ) {
+    steps = 0;
+    step_percentage++;
+    if ( step_percentage == step_percentage_toggle ) {
+      HAL_GPIO_TogglePin( HOME_GPIO_Port, HOME_Pin );
+    }
+  }
 }
 
-static int stepSize(uint8_t sz) {
+static int stepSize(step_size_t sz) {
   HAL_GPIO_WritePin( S_M0_GPIO_Port, S_M0_Pin, sz&0x01 );
   HAL_GPIO_WritePin( S_M1_GPIO_Port, S_M1_Pin, sz&0x02 );
   HAL_GPIO_WritePin( S_M2_GPIO_Port, S_M2_Pin, sz&0x04 );
@@ -143,10 +184,14 @@ static int stepSize(uint8_t sz) {
   return m[sz];
 }
 
-static void move(direction_t direction, int steps) {
-  HAL_GPIO_WritePin( S_NEN_GPIO_Port, S_NEN_Pin, 0 );
-  stepDirection( direction );
+static void move(step_dir_t dir, int steps, int opto_toggle) {
+  step_percentage = 0;
+  step_percentage_toggle = opto_toggle;
 
+  HAL_GPIO_WritePin( S_NEN_GPIO_Port, S_NEN_Pin, step_enable );
+  HAL_GPIO_WritePin( S_DIR_GPIO_Port, S_DIR_Pin, dir );
+  delayUs(10);
+  
   // accelerate...
   for (int j=5; j>=0; j--) {
     int m = stepSize( j );
@@ -167,34 +212,14 @@ static void move(direction_t direction, int steps) {
       stepSingle();
     }
   }
-}
-
-/*
-stepsPerRotation:200
-stepsPerInch:632.911392
-stepsPerLevel:1107.594937
-stepsPerCamDegree:12.306610
-
-every 100 steps look up table:
-accel
-flat1(8) 0(16) 0 0 0 0 0 0 0 0 1(90) 1 1 1 1 1 1 1 1 1 0(172) 1(180) 1 1 1 0(213) 0 0 0 0 0 0 1(270) 1 1 1 1 1 1 0(328) 0 0 0 
-decel
-*/
-
-static void optoSimulator(void) {
-  // convert the motor position to something the factory williams game
-  // thinks is the opto sensor shining through the slots on the rotating cam
-  int a = camSteps;// / (int)stepsPerCamDegree;  // approx 12 per degree.
-  if ( a < (int)(0*stepsPerCamDegree) )   { a += (int)(360*stepsPerCamDegree); }
-  if ( a > (int)(360*stepsPerCamDegree) ) { a -= (int)(360*stepsPerCamDegree); }
-  if ( ( a >= (int)(0*stepsPerCamDegree) && a <= (int)(11*stepsPerCamDegree) ) || 
-       ( a >= (int)(90*stepsPerCamDegree) && a <= (int)(168*stepsPerCamDegree) ) || 
-       ( a >= (int)(180*stepsPerCamDegree) && a <= (int)(212*stepsPerCamDegree) ) || 
-       ( a >= (int)(270*stepsPerCamDegree) && a <= (int)(326*stepsPerCamDegree) ) ) {
-    LL_GPIO_SetOutputPin( HOME_GPIO_Port, HOME_Pin );
-  } else {
-    LL_GPIO_ResetOutputPin( HOME_GPIO_Port, HOME_Pin );
-  }
+  
+  // simulate the "complete" event on the original cam
+  // it's closed for ccw moves and open for cw moves
+  HAL_GPIO_TogglePin( HOME_GPIO_Port, HOME_Pin );
+  
+  // stall a bit for the williams cpu to see that we completed the move
+  // allowing it to properly decide to disable or enable the dc motor enable signal
+  delayMs( 500 );
 }
 
 
@@ -211,28 +236,30 @@ int main(void)
   // NVIC_SetPriority(S_NFLT_EXTI_IRQn, 1, 0);
   NVIC_EnableIRQ(S_NFLT_EXTI_IRQn);
   
-  HAL_GPIO_WritePin( S_NEN_GPIO_Port, S_NEN_Pin, 0 );
-  HAL_GPIO_WritePin( S_NRST_GPIO_Port, S_NRST_Pin, 1 );
+  HAL_GPIO_WritePin( S_NEN_GPIO_Port, S_NEN_Pin, step_enable );
+  HAL_GPIO_WritePin( S_NRST_GPIO_Port, S_NRST_Pin, step_deassert );
 
   // move off the switch (we're probably on it)
-  int m = stepSize( 4 );
-  stepDirection( ccw );
+  int m = stepSize( step_size_4th );
+  HAL_GPIO_WritePin( S_DIR_GPIO_Port, S_DIR_Pin, step_dir_up );
   for (int i=0; i<stepsPerRotation*m; i++) {
     stepSingle();
   }
 
   // find the switch at slow speed
-  stepSize( 4 );
-  stepDirection( cw );
+  stepSize( step_size_16th );
+  HAL_GPIO_WritePin( S_DIR_GPIO_Port, S_DIR_Pin, step_dir_down );
   int ct = 0;
   do {
     stepSingle();
     if ( HAL_GPIO_ReadPin(LIMIT_GPIO_Port,LIMIT_Pin) == 1 ) { ct++; } else { ct=0; }
   } while ( ct < 4 ); // debounce
   
-  // we're homed at 0 degrees
-  optoSimulator(); // force zero cam angle
-  camSteps = 0;
+  // once we zero the stepper, we can allow the machine to "home the cam"
+  // machine will try to home the to cam CW down, where opto is open at complete
+  // just to the right of the little nub on the cam
+  current_level = level_down;
+  HAL_GPIO_WritePin( HOME_GPIO_Port, HOME_Pin, opto_open );
   
   fault = false;
   
@@ -240,36 +267,70 @@ int main(void)
     
     uint32_t tick = HAL_GetTick();
     static uint32_t inactivityTimer = 0;
-
-    // might be some accidental triggering here, depends on how long the 
-    // williams cpu takes to deassert the motor enable signal
+    
     int enable = !HAL_GPIO_ReadPin( EN_GPIO_Port, EN_Pin );
     int direction = HAL_GPIO_ReadPin( DIR_GPIO_Port, DIR_Pin );
+    
+    if ( debouncedSwitch( but_left ) ) {
+      enable = true;
+      direction = cam_dir_ccw;
+    }
+    if ( debouncedSwitch( but_right ) ) {
+      enable = true;
+      direction = cam_dir_cw;
+    }
+    
     if ( enable ) {
         inactivityTimer = tick;
-        if ( direction ) {
-          move( cw, stepsPerLevel );
+        
+        if ( direction == cam_dir_ccw ) {
+          switch ( current_level ) {
+            case level_down:
+              move( step_dir_up, stepsPerLevel, 10/*perc of move to open opto*/);
+              current_level = level_mid_r;
+              break;
+            case level_mid_r:
+              move( step_dir_up, stepsPerLevel, 90/*perc of move to open opto*/);
+              current_level = level_up;
+              break;
+            case level_up:
+              move( step_dir_down, stepsPerLevel, 33/*perc of move to open opto*/);
+              current_level = level_mid_l;
+              break;
+            case level_mid_l:
+              move( step_dir_down, stepsPerLevel, 66/*perc of move to open opto*/);
+              current_level = level_down;
+              break;
+            }
         } else {
-          move( ccw, stepsPerLevel );
+          switch ( current_level ) {
+            case level_down:
+              move( step_dir_up, stepsPerLevel, 33/*perc of move to close opto*/);
+              current_level = level_mid_l;
+              break;
+            case level_mid_r:
+              move( step_dir_up, stepsPerLevel, 66/*perc of move to close opto*/);
+              current_level = level_up;
+              break;
+            case level_up:
+              move( step_dir_down, stepsPerLevel, 10/*perc of move to close opto*/);
+              current_level = level_mid_r;
+              break;
+            case level_mid_l:
+              move( step_dir_down, stepsPerLevel, 90/*perc of move to close opto*/);
+              current_level = level_down;
+              break;
+            }
         }
+          
+          
     }
 
     if ( fault || tick-inactivityTimer > MINUTES_TO_TICKS(1)) {
       inactivityTimer = tick;
-      HAL_GPIO_WritePin( S_NEN_GPIO_Port, S_NEN_Pin, 1 );
+      HAL_GPIO_WritePin( S_NEN_GPIO_Port, S_NEN_Pin, step_disable );
     }
-    
-    if ( debouncedSwitch( left ) ) {
-      inactivityTimer = tick;
-      // move up a bit
-      move( cw, stepsPerLevel );
-    }
-    if ( debouncedSwitch( right ) ) {
-      inactivityTimer = tick;
-      // move down a bit
-      move( ccw, stepsPerLevel );
-    }
-      
+          
   }
   
 }
